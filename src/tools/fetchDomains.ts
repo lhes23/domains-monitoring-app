32a07a53babc8f8132fb3740189f11b1f4512aee
lhes23 <32542078+lhes23@users.noreply.github.com>
2025-02-23 15:11:37 +0800

    refactor code to use proxy server

--- src/tools/fetchDomains.ts
+++ src/tools/fetchDomains.ts
@@ -1,5 +1,4 @@
 import { TDomain } from '@/app/type'
-import { XMLParser } from 'fast-xml-parser'
 
 export const fetchGodaddyDomains = async () => {
   const res = await fetch(
@@ -29,13 +28,7 @@ export const fetchGodaddyDomains = async () => {
     .filter(
       (domain: TDomain) => domain.status?.toLowerCase() !== 'updated_ownership'
     )
-
-  const sortedDomains = activeDomains.sort((a: TDomain, b: TDomain) => {
-    if (!a.expires || !b.expires) return
-    return new Date(a.expires).getTime() - new Date(b.expires).getTime()
-  })
-
-  return sortedDomains
+  return activeDomains
 }
 
 export const fetchNamecheapDomains = async () => {
@@ -44,50 +37,14 @@ export const fetchNamecheapDomains = async () => {
   const username = process.env.NAMECHEAP_USERNAME
   const clientIp = process.env.NAMECHEAP_CLIENT_IP
 
-  let page = 1
-  let allDomains: TDomain[] = []
-  let totalPages = 1 // Default to 1, will be updated dynamically
-
-  const parser = new XMLParser({
-    ignoreAttributes: false,
-    attributeNamePrefix: '',
-    textNodeName: 'value'
+  const url = `${process.env.AWS_EC2_PROXY_URL}/namecheap?ApiUser=${apiUser}&ApiKey=${apiKey}&UserName=${username}&ClientIp=${clientIp}`
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' }
   })
-  while (page <= totalPages) {
-    const url = `${process.env.AWS_EC2_PROXY_URL}?ApiUser=${apiUser}&ApiKey=${apiKey}&UserName=${username}&ClientIp=${clientIp}&Command=namecheap.domains.getList&Page=${page}&PageSize=100`
 
-    const res = await fetch(url, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/xml' }
-    })
-
-    if (!res.ok) {
-      console.error(
-        `Error fetching page ${page}:`,
-        res.status,
-        await res.text()
-      )
-      break
-    }
-
-    const xmlText = await res.text()
-    const json = parser.parse(xmlText)
-    const domains =
-      json.ApiResponse.CommandResponse.DomainGetListResult.Domain || []
-    allDomains = [...allDomains, ...domains]
-
-    // Update total pages based on the response
-    const pagingInfo = json.ApiResponse.CommandResponse.Paging
-    const totalItems = parseInt(pagingInfo.TotalItems, 10)
-    const pageSize = parseInt(pagingInfo.PageSize, 10)
-    totalPages = Math.ceil(totalItems / pageSize)
-
-    console.log(
-      `Fetched page ${page} of ${totalPages} - Total domains so far: ${allDomains.length}`
-    )
-
-    page++
+  if (!res.ok) {
+    console.error(`Error fetching page`, res.status, await res.text())
   }
-
-  return allDomains
+  return res.json()
 }
